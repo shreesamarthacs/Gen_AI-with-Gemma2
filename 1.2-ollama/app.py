42a3caa05c06c1a7b3eeb15ac4de6c7ea12c3f31
shreesamarthacs <shreesamartha13@gmail.com>
2024-08-22 22:42:59 +0530

    Summary

--- 1.2-ollama/app.py
+++ 1.2-ollama/app.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-from langchain_community.llms import Ollama
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -19,13 +18,13 @@ os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are a expert in all fields. Please respond to the question asked"),
+        ("system","You are an expert. Please respond to the question asked"),
         ("user","Question:{question}")
     ]
 )
 
 ## streamlit framework
-st.title("GenAI With Gemma Model")
+st.title("GenAI With LLaMA Model")
 input_text=st.text_input("What question you have in mind?")
 
 
